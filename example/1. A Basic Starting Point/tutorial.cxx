a9346001e29317c023db6b0bfa27f26d8317e59d
Lellansin <lellansin@gmail.com>
2018-03-09 17:04:15 +0800

    example: update 1

--- example/1. A Basic Starting Point/tutorial.cxx
+++ example/1. A Basic Starting Point/tutorial.cxx
@@ -2,10 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "TutorialConfig.h"
+ 
 int main (int argc, char *argv[])
 {
   if (argc < 2)
     {
+    fprintf(stdout,"%s Version %d.%d\n",
+            argv[0],
+            Tutorial_VERSION_MAJOR,
+            Tutorial_VERSION_MINOR);
     fprintf(stdout,"Usage: %s number\n",argv[0]);
     return 1;
     }
